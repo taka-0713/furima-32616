e7f4636d1e657a25b8ab38537189de50662905bc
taka-0713 <ottake07@gmail.com>
2020-12-24 16:08:33 +0900

    FactoryBotの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    last_name_kanji {Faker::Name.last_name}
+    first_name_kanji {Faker::Name.first_name}
+    last_name_kana {Faker::Name.last_name}
+    first_name_kana {Faker::Name.first_name}
+    date_of_birth {Faker::Date.between
+  end
+end
