9c3258b919e8f1d6583c96b4258230ae9764a8f0
taka-0713 <ottake07@gmail.com>
2020-12-28 19:34:53 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  with_options extend ActiveHash::Associations::ActiveRecordExtensions do
+    belongs_to :category
+    belongs_to :item_status
+    belongs_to :shipping_cost
+    belongs_to :prefecture
+    belongs_to :shipping_date
+  end
+
 end
