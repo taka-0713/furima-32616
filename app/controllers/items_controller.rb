5e304725e14d744acf4d9cba295cb6e3c5d4d2a3
taka-0713 <ottake07@gmail.com>
2020-12-29 00:03:56 +0900

    出品した商品を保存できるよう実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,20 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # def create
-  # end
+  def create
+   @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :describe, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
   
 end
