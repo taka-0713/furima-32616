58ce22b1b0f58ddff01471177477d7ab30c4d707
taka-0713 <ottake07@gmail.com>
2020-12-28 23:26:01 +0900

    商品出品ページが表示できるよう実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  # def create
+  # end
   
 end
