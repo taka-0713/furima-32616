2deab246c28a6b2d3f165d8d62107874b15f8231
taka-0713 <ottake07@gmail.com>
2020-12-28 21:45:31 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,20 @@
 class Item < ApplicationRecord
 
-  belongs_to :user
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :describe
+  end
 
-  has_one_attached :image
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
+  validates :price, presence: true, inclusion: {in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
 
   with_options extend ActiveHash::Associations::ActiveRecordExtensions do
     belongs_to :category
@@ -11,5 +23,9 @@ class Item < ApplicationRecord
     belongs_to :prefecture
     belongs_to :shipping_date
   end
+
+  belongs_to :user
+
+  has_one_attached :image
 
 end
