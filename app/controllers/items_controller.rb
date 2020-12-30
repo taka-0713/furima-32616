e29417790075c7eee7484c5651e174850f793a98
taka-0713 <ottake07@gmail.com>
2020-12-30 16:06:56 +0900

    ログイン状態によるページの遷移と編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -24,9 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path(@item.id)
+     else
+      render :edit
+     end
   end
 
   private
