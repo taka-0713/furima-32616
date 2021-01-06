75b16c6c32e01b5fa2a840ff8f7dfab457227a2a
taka-0713 <ottake07@gmail.com>
2021-01-06 18:56:36 +0900

    addressesテーブルの作成

--- db/migrate/20210106093930_create_addresses.rb
+++ db/migrate/20210106093930_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.references  :order,          foreign_key: true
+      t.string      :postal_code,    null: false
+      t.integer     :prefecture_id,  null: false
+      t.string      :municipalities, null: false
+      t.string      :block_number,   null: false
+      t.string      :building_name
+      t.string      :phone_number,   null: false
       t.timestamps
     end
   end
